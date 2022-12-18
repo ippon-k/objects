1926cace162604535a8e48653e767cd07c24fb59
ichimoto <mentaikodondon@gmail.com>
2022-12-18 12:42:19 +0900

    test

--- add.php
+++ add.php
@@ -11,7 +11,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
     <title>objects</title>
 </head>
 
