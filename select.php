004d16a10155ff6d083938aff81a8d600b12f71b
ichimoto <mentaikodondon@gmail.com>
2022-12-18 12:44:53 +0900

    test1

--- select.php
+++ select.php
@@ -58,7 +58,7 @@ if ($status == false) {
         <nav class="navbar navbar-default">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="add.php">データ登録</a>
+                    <a class="navbar-brand" href="add.php">動産表示</a>
                 </div>
             </div>
         </nav>
