45e84fe2c786539ed073a8f9d5eafface50f0491
ichimoto <mentaikodondon@gmail.com>
2022-12-18 12:54:30 +0900

    boot

--- objects.php
+++ objects.php
@@ -20,7 +20,7 @@ if ($menu === '1') {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
     <title>objects</title>
 </head>
 
