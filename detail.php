caa8c09b3b464d07df426552c6cf8652f91a3a2d
ichimoto <mentaikodondon@gmail.com>
2023-01-02 19:09:52 +0900

    dt

--- detail.php
+++ detail.php
@@ -18,7 +18,7 @@ try {
 
 //２．データ取得SQL作成
 $stmt = $pdo->prepare("SELECT * FROM gs_bm_table WHERE id = :id;");
-$stmt->bindValue(':id', $ic, PDO::PARAM_INT);
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
 
 $status = $stmt->execute();
 
@@ -32,82 +32,69 @@ if ($status === false) {
     $result = $stmt->fetch();
 }
 
-// -------------------------------
-//データ表示
-$view = "";
-if ($status == false) {
-    //execute（SQL実行時にエラーがある場合）
-    $error = $stmt->errorInfo();
-    exit("ErrorQuery:" . $error[2]);
-} else { //elseの中はsql実行成功した場合
-    //Selectデータの数だけ自動でループしてくれる
-    //FETCH_ASSOC=http://php.net/manual/ja/pdostatement.fetch.php
-    while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) { //一行とってくるよ
-
-        $view .= '<tr><td>' . $result['id'] . '</td><td>' . h($result['name']) . '</td><td>' . h($result['category']) . '</td><td>' . $result['date'] . '</td><td>' . h($result['place']) . '</td><td>' . h($result['check1']) . '</td><td>' . h($result['control_num']) . '</td><td>' . h($result['amortization_period']) . '</td><td>'  . h($result['acquisition_cost']) . '</td><td>' . h($result['residual_value']) . '</td><td>' . h($result['others']) . '</td></tr>';
-    }
-}
 ?>
 
 
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>動産一覧</title>
-    <link rel="stylesheet" href="css/range.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style1.css">
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/style1.css">
-    <style>
-        div {
-            padding: 10px;
-            font-size: 16px;
-        }
-    </style>
+    <title>objects</title>
 </head>
 
-<body id="main">
-    <!-- Head[Start] -->
-    <header>
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="add.php">動産表示</a>
-                </div>
-            </div>
-        </nav>
-    </header>
-    <!-- Head[End] -->
-
-    <!-- Main[Start] -->
+<body>
     <div>
-        <div class="container jumbotron">
-            <table class="table1">
-                <tr>
-                    <th>ID</th>
-                    <th>品名</th>
-                    <th>カテゴリ</th>
-                    <th>購入日</th>
-                    <th>保管場所</th>
-                    <th>次回確認</th>
-                    <th>管理番号</th>
-                    <th>償却期間</th>
-                    <th>取得価額</th>
-                    <th>最終残価</th>
-                    <th>その他</th>
-                </tr>
-                <?= $view ?>
-            </table>
-        </div>
+        <header>
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="add.php">動産詳細</a>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <h1>動産登録</h1>
+        <form action="insert.php" method="post">
+            <label for="name">品名</label><br>
+            <input type="text" name="name" id="name" required value="<?= $result['name'] ?>"><br>
+
+            <label for="category">カテゴリ</label><br>
+            <input type="text" name="category" id="category" required value="<?= $result['category'] ?>"><br>
+
+            <label for="purchase">購入日</label><br>
+            <input type="date" name="date" id="date" required value="<?= $result['date'] ?>"><br>
+
+            <label for="place">保管場所</label><br>
+            <input type="text" name="place" id="place" value="<?= $result['place'] ?>"><br>
+
+            <label for="checkPoint">チェックポイント</label><br>
+            <input type="date" name="check1" id="check1" value="<?= $result['check1'] ?>"><br>
+
+            <label for="num">管理番号</label><br>
+            <input type="text" name="control_num" id="control_num" value="<?= $result['control_num'] ?>"><br>
+
+            <label for="amortization_period">償却期間（年）</label><br>
+            <input type="number" name="amortization_period" id="amortization_period" value="<?= $result['amortization_period'] ?>"><br>
+
+            <label for="acquisition_cost">取得価額</label><br>
+            <input type="number" name="acquisition_cost" id="acquisition_cost" value="<?= $result['acquisition_cost'] ?>"><br>
+
+            <label for="residual_value">最終残価率</label><br>
+            <input type="number" name="residual_value" id="residual_value" value="<?= $result['residual_value'] ?>"><br>
+
+            <label for="others">その他</label><br>
+            <input type="text" name="others" id="others" value="<?= $result['others'] ?>"><br>
+
+            <input type="submit" value="send">
+        </form>
+        <p><a href="index.html">return</a></p>
     </div>
-    <!-- Main[End] -->
-    <button><a href="index.html">to menu</a></button>
-    <button><a href="add.php">to add</a></button>
-
-
 </body>
 
 </html>
